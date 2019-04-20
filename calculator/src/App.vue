421ad816883cebbf1b4496ca9b87f1b408df9e97
Arthur Ferrão <arthurdeferrao@gmail.com>
2019-04-20 20:09:15 -0300

    Inicializa calculator

---
+++ calculator/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="app">
+        <h1>Teste Vue!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+
+<style>
+
+body {
+    margin: 0;
+}
+
+#app {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+
+    color: #fff;
+    background: linear-gradient(to right, rgb(83, 105, 118), rgb(41, 46, 73));   
+}
+
+</style>
